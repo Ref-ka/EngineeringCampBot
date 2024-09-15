2d7d2c17a9defcd7da92fb8462ac35e1989144ca
Vladimir Krokhin <119413075+VladimirKrokhin@users.noreply.github.com>
2024-09-15 11:37:51 +0500

    Update day_2_1.py
    Разделил интерфейс обработки /start и приветствие (/greet).
    IMHO, так нагляднее.

--- practices/day_2_1.py
+++ practices/day_2_1.py
@@ -8,7 +8,14 @@ bot = telebot.TeleBot(API_TOKEN)
 # Обработчик команды /start
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я ваш дружелюбный бот. Как я могу помочь вам сегодня?")
+    bot.reply_to(message, "Привет! Я ваш дружелюбный бот. Для взаимодействия используйте команды /start или /bye.")
+
+# Обработчик команды /greet
+@bot.message_handler(commands=['greet'])
+def welcome(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id,
+                     'Добро пожаловать в дружелюбного бота. Рад вас видеть! Чем я могу быть вам сегодня полезен?')
 
 # Обработчик команды /bye
 @bot.message_handler(commands=['bye'])
@@ -18,7 +25,7 @@ def send_goodbye(message):
 # Обработчик текстовых сообщений
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.reply_to(message, "Извините, я вас не понял. Попробуйте использовать команды /start или /bye.")
+    bot.reply_to(message, "Извините, я вас не понял. Попробуйте использовать команды /greet или /bye.")
 
 # Запуск бота
 bot.infinity_polling()
