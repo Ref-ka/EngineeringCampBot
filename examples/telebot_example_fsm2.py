ceb50f0c78e3b2442f7fcfd622c7045ec425d475
Ref_ka <228refka1337@gmail.com>
2024-09-20 19:42:42 +0500

    доделан пример по машине состояний соответственно схеме в документации

--- examples/telebot_example_fsm2.py
+++ examples/telebot_example_fsm2.py
@@ -12,11 +12,12 @@ bot = telebot.TeleBot(API_TOKEN, state_storage=state_storage)
 class TaskStates(StatesGroup):
     adding_name = State()
     adding_surname = State()
+    confirm = State()
 
 # Команда /start
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Я могу запомнить твои имя и фамилию.")
+    bot.send_message(message.chat.id, "Привет! Я могу запомнить твои имя и фамилию. Используйте команду /add")
 
 # Команда /add для запоминания
 @bot.message_handler(commands=['add'])
@@ -30,7 +31,7 @@ def cancel(message):
     bot.delete_state(message.from_user.id, message.chat.id)
     bot.send_message(message.chat.id, "Действие отменено. Вы можете начать заново, используя команду /add.")
 
-# Обработка состояния добавления названия задачи
+# Обработка состояния добавления имени
 @bot.message_handler(state=TaskStates.adding_name)
 def enter_title(message):
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
@@ -38,12 +39,23 @@ def enter_title(message):
     bot.set_state(message.from_user.id, TaskStates.adding_surname, message.chat.id)
     bot.send_message(message.chat.id, "Введите вашу фамилию:")
 
-# Обработка состояния добавления описания задачи
+# Обработка состояния добавления фамилии
 @bot.message_handler(state=TaskStates.adding_surname)
 def enter_description(message):
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         data['surname'] = message.text
-        bot.send_message(message.chat.id, f"Отлично, я всё запомнил вас зовут: {data['name']} {data['surname']}")
+    bot.set_state(message.from_user.id, TaskStates.confirm, message.chat.id)
+    bot.send_message(message.chat.id, f"Подтверждение обработки запроса.\n"
+                                      "Для подтверждения введите 'да', для отмены /cancel")
+
+# Обработка состояния подтверждения
+@bot.message_handler(state=TaskStates.confirm)
+def confirm(message):
+    if message.text == 'да':
+        with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+            bot.send_message(message.chat.id, f"Отлично, я всё запомнил. Вас зовут: {data['name']} {data['surname']}")
+    else:
+        bot.reply_to(message, "Вам нужно ввести 'да' для подтверждения, /cancel для отмены.")
 
 # Добавляем фильтр состояний
 bot.add_custom_filter(StateFilter(bot))
